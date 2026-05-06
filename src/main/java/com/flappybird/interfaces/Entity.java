dd80aa5b6471e1a3193b7c3d996a6d2cc55bf045
miguel-124 <mikicuellar20@gmail.com>
2026-05-06 01:22:18 -0400

    Updated Entity class. Adding Sprite prop

--- src/main/java/com/flappybird/interfaces/Entity.java
+++ src/main/java/com/flappybird/interfaces/Entity.java
@@ -5,15 +5,18 @@ import com.flappybird.utils.Vector2;
 
 public abstract class Entity {
     public Vector2 position;
-    public Rectangle sourceRectangle; // Para mapear los sprites
+    public Vector2 scale;
+    public Sprite sprite;
 
     public Rectangle getDimensions(){
+        var sourceRectangle = sprite.sourceRectangle;
+
         return
             new Rectangle(position.x(), position.y(), sourceRectangle.WIDTH, sourceRectangle.HEIGHT);
     }
 
-    public Entity(Vector2 position, Rectangle souRectangle){
+    public Entity(Vector2 position, Sprite sprite){
         this.position = position;
-        this.sourceRectangle = souRectangle;
+        this.sprite = sprite;
     }
 }
---
+++ src/main/java/com/flappybird/interfaces/Sprite.java
@@ -0,0 +1,13 @@
+package com.flappybird.interfaces;
+
+import com.flappybird.utils.Rectangle;
+
+public class Sprite {
+    // Texture texture;
+    public Rectangle sourceRectangle;
+
+    // public Sprite(Texture texture, Rectangle sourceRectangle) {
+    //     this.texture = texture;
+    //     this.sourceRectangle = sourceRectangle;
+    // }
+}
