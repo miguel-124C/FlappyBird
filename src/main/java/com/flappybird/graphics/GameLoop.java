2b75cdda5e12624ba5f306dbde4abb8812c96278
miguel-124 <mikicuellar20@gmail.com>
2026-05-05 23:54:01 -0400

    Struct base in GameLoop

---
+++ src/main/java/com/flappybird/graphics/GameLoop.java
@@ -0,0 +1,20 @@
+package com.flappybird.graphics;
+
+public class GameLoop {
+
+    public GameLoop(){
+        // var graphics = ;
+    }
+
+    public void run(){
+        // game.initialize();
+        // game.loadContent();
+
+        while ( 0 < 1 ) {
+            // var deltaTime = ;
+            // game.update(deltaTime);
+            // game.draw(deltaTime);
+        }
+    }
+
+}
