36746bec0c8b328293f3536bef656424bc5981bb
miguel-124 <mikicuellar20@gmail.com>
2026-05-08 00:54:29 -0400

    Refactor min and max height_pipe

--- src/main/java/com/flappybird/factories/PipeFactory.java
+++ src/main/java/com/flappybird/factories/PipeFactory.java
@@ -8,21 +8,28 @@ import com.flappybird.utils.*;
 
 public class PipeFactory {
 
-    private final float GAP_BETWEEN_PIPES = 50f;
-    private final float MAX_HEIGHT_PIPE   = 150;
-    private final float MIN_HEIGHT_PIPE   = 20;
+    private final float GAP_BETWEEN_PIPES = 200f;
+    private final float MAX_HEIGHT_PIPE;
+    private final float MIN_HEIGHT_PIPE;
 
     private final int LIMIT_WIDTH = Constants.screenWidth;
     private final int LIMIT_HEGHT = Constants.screenHeight;
 
+    private final int WIDTH_PIPE = 48;
+
     private final Random random = new Random();
+
+    public PipeFactory(){
+        MIN_HEIGHT_PIPE = 50f;
+        MAX_HEIGHT_PIPE = Constants.screenHeight - GAP_BETWEEN_PIPES - MIN_HEIGHT_PIPE;
+    }
 
     public PipeEntity spawnPipe() {
         var position = new Vector2(LIMIT_WIDTH, 0);
 
         var texture = new Texture("");
         var heightSource = random.nextFloat(MIN_HEIGHT_PIPE, MAX_HEIGHT_PIPE);
-        var sourceRectangle = new Rectangle(303, 0, 24, heightSource);
+        var sourceRectangle = new Rectangle(303, 0, WIDTH_PIPE, heightSource);
 
         var sprite = new Sprite(texture, sourceRectangle);
         return new PipeEntity(position, sprite);
@@ -37,7 +44,7 @@ public class PipeFactory {
 
         var texture = new Texture("");
         var heightSource = LIMIT_HEGHT - ySecondPipe;
-        var sourceRectangle = new Rectangle(330, 0, 24, heightSource);
+        var sourceRectangle = new Rectangle(330, 0, WIDTH_PIPE, heightSource);
 
         var sprite = new Sprite(texture, sourceRectangle);
         return new PipeEntity(position, sprite);
