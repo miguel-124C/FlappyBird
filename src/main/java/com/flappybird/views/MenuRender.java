5b924612f1b4e7657a451d618efc170fec60a957
Miguel <mikicuellar20@gmail.com>
2026-05-08 18:31:13 -0400

    Implement MenuRender class and define IRender methods

--- src/main/java/com/flappybird/views/MenuRender.java
+++ src/main/java/com/flappybird/views/MenuRender.java
@@ -1,5 +1,24 @@
 package com.flappybird.views;
 
-public class MenuRender {
+import com.flappybird.graphics.SpriteRenderer;
+
+public class MenuRender implements IRender {
+
+    @Override
+    public void draw(SpriteRenderer spriteRender) {
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+    @Override
+    public void cleanUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cleanUp'");
+    }
     
 }
