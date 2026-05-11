227316f6a4f78454073695f801bf7fa5767909c1
Miguel <mikicuellar20@gmail.com>
2026-05-11 15:11:37 -0400

    Refactor InputManager to use ConfigCore for game state and streamline player input handling

--- src/main/java/com/flappybird/controllers/InputManager.java
+++ src/main/java/com/flappybird/controllers/InputManager.java
@@ -2,39 +2,50 @@ package com.flappybird.controllers;
 
 import org.lwjgl.glfw.GLFW;
 
-import com.flappybird.models.GameCore;
+import com.flappybird.core.ConfigCore;
 
 public class InputManager {
 
     private long window;
-    private GameCore gameCore;
-    private boolean prevSpace;
     
-    public void initialize(long window, GameCore gameCore){
+    public InputManager(){
+    }
+
+    public void initialize(long window){
         this.window = window;
-        this.gameCore = gameCore;
     }
     
     public void update(float deltaTime){
-        switch (gameCore.getState()) {
+        switch (ConfigCore.getInstance().gameState) {
             case PLAYING:
-                handleSpace(deltaTime);
+                handleInGame(deltaTime);
                 break;
+            case PAUSE:
+                handleInPause(deltaTime);
             default:
                 break;
         }
     }
 
-    public void handleSpace(float deltaTime){
+    public void handleInGame(float deltaTime){
         if (isPressed(GLFW.GLFW_KEY_ESCAPE))
             GLFW.glfwSetWindowShouldClose(window, true);
 
-        boolean spaceAhora = isPressed(GLFW.GLFW_KEY_SPACE);
-        if (spaceAhora && !prevSpace) {
-            var bird = gameCore.world.Bird;
-            bird.jump(deltaTime);
+        var players = ConfigCore.getInstance().getPlayers();
+        for (var player : players) {
+            var gameControl = player.GAME_CONTROL;
+            int keyJump = gameControl.getKeyJump();
+            boolean keyJumpNow = isPressed(keyJump);
+
+            if (keyJumpNow && !gameControl.prevKey) {
+                player.jump();
+            }
+            gameControl.prevKey = keyJumpNow;
         }
-        prevSpace = spaceAhora;
+    }
+
+    private void handleInPause(float deltaTime){
+        
     }
 
     private boolean isPressed(int key){
