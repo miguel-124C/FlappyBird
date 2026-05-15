2510fbbb1a847864dbe93653cb2e7902f079713d
Miguel <mikicuellar20@gmail.com>
2026-05-15 00:27:28 -0400

    HudCore class created

---
+++ src/main/java/com/flappybird/core/HudCore.java
@@ -0,0 +1,13 @@
+package com.flappybird.core;
+
+public class HudCore implements ICore {
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void update(float deltaTime) {
+    }
+
+}
